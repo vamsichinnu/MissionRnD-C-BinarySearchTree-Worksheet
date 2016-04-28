99fb9ba24e3efcac61235cedc590fb7d157337ab
vamsichinnu <vamsichinnu@missionrnd.com>
2016-04-28 18:21:57 +0530

    Submit Commit

--- src/BSTRows.cpp
+++ src/BSTRows.cpp
@@ -20,6 +20,7 @@ Output :Return An array which has All the elements in row by row order (R->L) .
 Note : Return -1 for Invalid Cases .
 */
 
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -29,9 +30,60 @@ struct node{
 	struct node *right;
 };
 
-
-
+int get_height1(struct node *root){
+	int leftheight, rightheight;
+	if (root == NULL)
+		return 0;
+	else
+	{
+		leftheight = get_height1(root->left);
+		rightheight = get_height1(root->right);
+		if (leftheight >rightheight)
+		{
+			return leftheight + 1;
+		}
+		else
+		{
+			return rightheight + 1;
+		}
+	}
+	return 0;
+}
+void adding(struct node *root, int i, int *output, int *j)
+{
+	int k;
+	if (root==NULL)
+	{
+		return;
+	}
+	if (i == 1)
+	{
+		k = (*j)++;
+		output[k] = root->data;
+	}
+	else
+	{
+			adding(root->right, i-1, output, j);
+			adding(root->left, i-1, output, j);
+	}
+	
+}
 int* BSTRighttoLeftRows(struct node* root)
 {
-    return NULL;
+		int *output, i, height, j = 0;
+		struct node *temp;
+		temp = (struct node *)malloc(sizeof(struct node) * 1);
+		temp = root;
+		output = (int *)malloc(sizeof(int)* 100);
+		if (root == NULL)
+			return NULL;
+		else
+		{
+			height = get_height1(temp);
+			for (i = 1; i <=height; i++)
+			{
+				adding(root, i, output, &j);
+			}
+		}
+		return output;
 }
